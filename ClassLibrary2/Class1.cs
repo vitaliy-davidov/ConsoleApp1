ed75006f147fc3f037ce8fde3aef6e82604ec683
Vitaliy <Vitaliy@gmail.com>
2021-04-24 13:30:15 +0300

    jhjy

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class Class2
+    {
+        public int MyProperty2 { get; set; }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Main");
+            Class2 Obj2 = new Class2();
+            Obj2.MyProperty2 = 55;
         }
     }
 }
